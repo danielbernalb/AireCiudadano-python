945f0f39c7954c73cd47435a6655280f8ab008bf
danielbernalb <danielbernalb@gmail.com>
2024-06-22 13:27:01 -0500

    thanos

--- p4thanos.py
+++ p4thanos.py
@@ -34,11 +34,13 @@ def get_data(url, selected_cols):
     # list of values or single value in data response
     if 'values' in df.columns:
         df = df.explode('values')
-        df['timestamp'] = df['values'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]))
+        df['date'] = df['values'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]).date())
+        df['time'] = df['values'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]).time())
         df['value'] = df['values'].apply(lambda x: x[1])
         df = df.drop(columns="values")
     elif 'value' in df.columns:
-        df['timestamp'] = df['value'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]))
+        df['date'] = df['value'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]).date())
+        df['time'] = df['value'].apply(lambda x: datetime.datetime.utcfromtimestamp(x[0]).time())
         df['value'] = df['value'].apply(lambda x: x[1])
     
     df = df.rename(columns={
@@ -55,13 +57,11 @@ def get_data(url, selected_cols):
     # convert df to wide table
     df_result = _wide_table(df, selected_cols)
     
-    # set format and replace zero values in lat-lon columns if they are selected
+    # set format and replace zero values in lat-lon columns
     for col in selected_cols:
         df_result[col] = df_result[col].astype(float)
-    if 'Latitude' in df_result.columns:
-        df_result['Latitude'].replace(0, np.nan, inplace=True)
-    if 'Longitude' in df_result.columns:
-        df_result['Longitude'].replace(0, np.nan, inplace=True)
+    df_result['Latitude'].replace(0, np.nan, inplace=True)
+    df_result['Longitude'].replace(0, np.nan, inplace=True)
 
     return df_result
 
@@ -69,13 +69,18 @@ def get_data(url, selected_cols):
 def _wide_table(df, selected_cols):
     df_result = pd.pivot(
         df, 
-        index=['station', 'timestamp'], 
+        index=['station', 'date', 'time'], 
         columns='metric_name', 
         values='value'
     ).reset_index()
 
+    # Ensure all selected columns are present in the dataframe
+    for col in selected_cols:
+        if col not in df_result.columns:
+            df_result[col] = np.nan
+
     df_result = df_result[
-        ['station', 'timestamp'] + selected_cols
+        ['station', 'date', 'time'] + selected_cols
     ].reset_index(drop=True)
 
     df_result.columns.name = ""
@@ -86,6 +91,7 @@ def _wide_table(df, selected_cols):
 def _get_step(number, choice):
     # convert word to code
     options = {
+        "seconds": "s",
         "minutes": "m",
         "hours": "h",
         "days": "d",
@@ -115,17 +121,11 @@ def index():
             <input type="radio" id="time_range" name="option" value="time_range">
             <label for="time_range">Time range</label><br><br>
 
-            <label for="stats_option">Select Step Option:</label><br>
+            <label for="stats_option">Select Statistics Option:</label><br>
             <input type="radio" id="step_number" name="stats_option" value="step_number" checked>
             <label for="step_number">Step Number</label><br>
             <input type="radio" id="step_stats" name="stats_option" value="step_stats">
             <label for="step_stats">Step Statistics</label><br><br>
-
-            <label for="selected_cols">Select Variables to Analyze:</label><br>
-            {% for col in selected_cols %}
-            <input type="checkbox" id="{{ col }}" name="selected_cols" value="{{ col }}" checked>
-            <label for="{{ col }}">{{ col }}</label><br>
-            {% endfor %}<br>
 
             <label for="starts_day">Start Date:</label><br>
             <input type="date" id="starts_day" name="starts_day" value="{{ today }}"><br><br>
@@ -141,6 +141,7 @@ def index():
             <input type="number" id="step_number" name="step_number" value="1"><br><br>
             <label for="step_option">Step Option:</label><br>
             <select id="step_option" name="step_option">
+                <option value="seconds">Seconds</option>
                 <option value="minutes">Minutes</option>
                 <option value="hours" selected>Hours</option>
                 <option value="days">Days</option>
@@ -148,24 +149,35 @@ def index():
                 <option value="years">Years</option>
             </select><br><br>
 
+            <label for="variables">Select Variables to Analyze:</label><br>
+            <select id="variables" name="variables" multiple>
+                {% for col in selected_cols %}
+                <option value="{{ col }}">{{ col }}</option>
+                {% endfor %}
+            </select><br><br>
+
             <input type="submit" value="Retrieve Data">
         </form>
     </body>
     </html>
     '''
-    return render_template_string(html, selected_cols=selected_cols, today=str(datetime.date.today()))
+    return render_template_string(html, today=str(datetime.date.today()), selected_cols=selected_cols)
 
 @app.route('/api/data', methods=['GET'])
-def get_prometheus_data():
+def get_thanos_data():
     option = request.args.get('option', default='last_register', type=str)
     stats_option = request.args.get('stats_option', default='step_number', type=str)
     starts_day = request.args.get('starts_day', default=str(datetime.date.today()), type=str)
     starts_hour = request.args.get('starts_hour', default='00:00', type=str)
     ends_day = request.args.get('ends_day', default=str(datetime.date.today()), type=str)
-    ends_hour = request.args.get('ends_hour', default='00:00', type=str)
+    ends_hour = request.args.get('ends_hour', default='00:00', type:str)
     step_number = request.args.get('step_number', default=1, type=int)
     step_option = request.args.get('step_option', default='hours', type=str)
-    selected_vars = request.args.getlist('selected_cols')
+    variables = request.args.getlist('variables')
+
+    # Si no se seleccionan variables, se usan todas las columnas por defecto
+    if not variables:
+        variables = selected_cols
 
     try:
         # query to get all data
@@ -173,7 +185,7 @@ def get_prometheus_data():
 
         # last registers selected
         if option == 'last_register':
-            url = f"http://sensor.aireciudadano.com:30000/api/v1/query?query={query}"
+            url = f"http://194.242.56.226:30001/api/v1/query?query={query}"
         # range of time selected
         elif option == 'time_range':
             # construct start_datetime
@@ -182,31 +194,22 @@ def get_prometheus_data():
             end_datetime = f"{ends_day}T{ends_hour}:00Z"
             # construct step                
             step = _get_step(step_number, step_option)
-            url = f"http://sensor.aireciudadano.com:30000/api/v1/query_range?query={query}&start={start_datetime}&end={end_datetime}&step=1m"
+            url = f"http://194.242.56.226:30001/api/v1/query_range?query={query}&start={start_datetime}&end={end_datetime}&step={step}"
         
         # get obs from API, using the url created before
-        obs = get_data(url, selected_vars)
-
+        obs = get_data(url, variables)
+        
+        # Perform step statistics if selected
         if stats_option == 'step_stats':
-            # Resample to desired step and calculate statistics per station
-            obs.set_index(['timestamp'], inplace=True)
-            resampled_list = []
-
-            for station, group in obs.groupby('station'):
-                resampled = group.resample(step, on='timestamp').agg({
-                    col: ['mean', 'max', 'min', 'last'] for col in selected_vars
-                }).reset_index()
-                resampled['station'] = station
-                resampled_list.append(resampled)
-
-            obs = pd.concat(resampled_list, axis=0)
-            # Flatten the column MultiIndex
-            obs.columns = ['_'.join(col).strip() if col[1] else col[0] for col in obs.columns.values]
+            obs_stats = obs.describe().to_dict()
 
         # convert dataframe to json
         data_json = obs.to_json(orient='records')
         
-        return jsonify({'status': 'success', 'data': data_json}), 200
+        if stats_option == 'step_stats':
+            return jsonify({'status': 'success', 'data': data_json, 'stats': obs_stats}), 200
+        else:
+            return jsonify({'status': 'success', 'data': data_json}), 200
 
     except ValueError:
         return jsonify({'status': 'error', 'message': 'Nothing found.'}), 404
