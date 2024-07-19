c71a149c691c8be0bafd955e2cbd21cf86081bae
danielbernalb <danielbernalb@gmail.com>
2024-07-18 21:49:55 -0500

    5001

--- p56.py
+++ p56.py
@@ -198,4 +198,4 @@ def data():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
