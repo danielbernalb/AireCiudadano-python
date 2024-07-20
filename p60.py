284cdf1311b8131aa1fdb1ce60585a559838fa8b
danielbernalb <danielbernalb@gmail.com>
2024-07-19 23:19:34 -0500

    cambios min

--- p60.py
+++ p60.py
@@ -154,7 +154,8 @@ def data():
 
     # Adjust start_datetime to be one hour earlier
     start_datetime = f"{start_date}T{start_time}:00Z"
-    start_datetime = (datetime.datetime.fromisoformat(start_datetime[:-1]) - datetime.timedelta(hours=1)).isoformat() + 'Z'
+    #start_datetime = (datetime.datetime.fromisoformat(start_datetime[:-1]) - datetime.timedelta(hours=1)).isoformat() + 'Z'
+    start_datetime = (datetime.datetime.fromisoformat(start_datetime[:-1]) - datetime.timedelta(hours=1) + datetime.timedelta(minutes=1)).isoformat() + 'Z'
     end_datetime = f"{end_date}T{end_time}:00Z"
 
     if aggregation_method == 'average':
