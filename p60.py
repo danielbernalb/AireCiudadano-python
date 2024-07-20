24deec970b2aebde6caec19f8f1bb04977c639b6
danielbernalb <danielbernalb@gmail.com>
2024-07-19 22:42:34 -0500

    cambios

--- p60.py
+++ p60.py
@@ -20,7 +20,6 @@ def get_data(url, selected_cols):
     try:
         app.logger.debug(f'Request URL: {url}')
         response = requests.get(url)
-        #app.logger.debug(f'Response: {response.text}')
         data = response.json().get('data', {}).get('result', [])
         df = pd.json_normalize(data)
 
@@ -90,7 +89,7 @@ def index():
 
     return render_template_string('''
         <form action="/dataresult" method="post">
-            <label for="variables">Select variables 69:</label><br>
+            <label for="variables">Select variables 70:</label><br>
             <input type="checkbox" id="select_all" onclick="toggle(this);">
             <label for="select_all">Select/Deselect All</label><br>
             {% for col in selected_cols %}
@@ -155,9 +154,9 @@ def data():
     aggregation_method = request.form['aggregation_method']
     station_filter = request.form.get('station_filter', '')
 
-    app.logger.debug(f'Start_time: {start_time}')
     start_datetime = pd.to_datetime(f"{start_date}T{start_time}:00Z") - pd.Timedelta(hours=1) + pd.Timedelta(minutes=1)
     start_datetime_str = start_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+    app.logger.debug(f'Start_time: {start_time}')
     app.logger.debug(f'Adjusted start_datetime: {start_datetime_str}')
     end_datetime = f"{end_date}T{end_time}:00Z"
 
@@ -172,10 +171,10 @@ def data():
         obs = get_data(url, variables)
         if station_filter:
             filters = station_filter.split(',')
-            obs = obs[obs['station'].isin(filters)]
+            obs = obs[obs['station'].str.contains('|'.join(filters), case=False, na=False)]
 
-        total_records = 0
         if aggregation_method == 'average':
+            obs['date'] = pd.to_datetime(obs['date'])
             obs = obs.set_index(['station', 'date'])
             hourly_obs = []
 
