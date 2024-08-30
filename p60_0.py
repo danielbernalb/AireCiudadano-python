9054c30e78500a62ad74bf3b5641d393288fe2f8
danielbernalb <danielbernalb@gmail.com>
2024-08-30 18:28:11 -0500

    camb

--- p60_0.py
+++ p60_0.py
@@ -87,7 +87,7 @@ def index():
 
     return render_template_string('''
         <form action="/dataresult" method="post">
-            <label for="variables">Select variables 73:</label><br>
+            <label for="variables">Select variables 74:</label><br>
             <input type="checkbox" id="select_all" onclick="toggle(this);">
             <label for="select_all">Select/Deselect All</label><br>
             {% for col in selected_cols %}
