1c76f89118214c705b5eb0e9e95dd74bf6565cbc
danielbernalb <danielbernalb@gmail.com>
2024-09-09 10:20:56 -0500

    cambios

--- p1union2.py
+++ p1union2.py
@@ -108,7 +108,7 @@ def index():
 
     return render_template_string('''
         <form action="/dataresult" method="post">
-            <label for="variables">Select variables p1union2 7:</label><br>
+            <label for="variables">Select variables p1union2 8:</label><br>
             <input type="checkbox" id="select_all" onclick="toggle(this);">
             <label for="select_all">Select/Deselect All</label><br>
             {% for col in selected_cols %}
