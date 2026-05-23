a3d7f32e5d13aa981e7c05cca8cd80c3721e3bc6
Daniel <42117424+danielbernalb@users.noreply.github.com>
2026-05-23 11:30:07 -0500

    8084

--- app_DesdeCompuLocalVM_23may2026.py
+++ app_DesdeCompuLocalVM_23may2026.py
@@ -250,4 +250,4 @@ def data():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8084)
